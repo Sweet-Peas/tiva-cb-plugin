c6f439fd2cb57fef51bde9b0f04f2aa5e22ffe4b
Pontus Oldberg <pontus.oldberg@invector.se>
2014-04-06 12:34:54 +0200

    First commit, not working yet

---
+++ files/main.c
@@ -0,0 +1,18 @@
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+
+
+
+int main(void){
+  
+ 
+  while(1){
+    
+  
+    
+  };
+  
+}
